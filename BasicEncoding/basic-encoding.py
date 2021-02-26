d8691fa44427bae685ee1aa68758928fa68dc274
IngridAtMicrosoft <ingrid.henkel@microsoft.com>
2021-02-26 12:56:08 -0800

    edits

--- BasicEncoding/basic-encoding.py
+++ BasicEncoding/basic-encoding.py
@@ -57,103 +57,104 @@ credentials = AdalAuthentication(
     key
 )
 
-#The file path of file you want to upload
+# The file you want to upload.  For this example, put the file in the same folder as this script. 
+# The file ignite.mp4 has been provided for you. 
 source_file = "ignite.mp4"
 
-#Generate a random number that will be added to the naming of things so that you don't have to keep doing this during testing.
+# Generate a random number that will be added to the naming of things so that you don't have to keep doing this during testing.
 thisRandom = random.randint(0,9999)
 
-#Set the attributes of the input Asset using the random number
+# Set the attributes of the input Asset using the random number
 in_asset_name = 'inputassetName' + str(thisRandom)
 in_alternate_id = 'inputALTid' + str(thisRandom)
 in_description = 'inputdescription' + str(thisRandom)
 # Create an Asset object
 # From the SDK
-#Asset(*, alternate_id: str = None, description: str = None, container: str = None, storage_account_name: str = None, **kwargs) -> None
+# Asset(*, alternate_id: str = None, description: str = None, container: str = None, storage_account_name: str = None, **kwargs) -> None
 # The asset_id will be used for the container parameter for the storage SDK after the asset is created by the AMS client.
 input_asset = Asset(alternate_id=in_alternate_id,description=in_description)
 
-#Set the attributes of the output Asset using the random number
+# Set the attributes of the output Asset using the random number
 out_asset_name = 'outputassetName' + str(thisRandom)
 out_alternate_id = 'outputALTid' + str(thisRandom)
 out_description = 'outputdescription' + str(thisRandom)
 # From the SDK
-#Asset(*, alternate_id: str = None, description: str = None, container: str = None, storage_account_name: str = None, **kwargs) -> None
+# Asset(*, alternate_id: str = None, description: str = None, container: str = None, storage_account_name: str = None, **kwargs) -> None
 output_asset = Asset(alternate_id=out_alternate_id,description=out_description)
 
-#The AMS Client
+# The AMS Client
 print("Creating AMS client")
-#From SDK
-#AzureMediaServices(credentials, subscription_id, base_url=None)
+# From SDK
+# AzureMediaServices(credentials, subscription_id, base_url=None)
 client = AzureMediaServices(credentials, subscription_id)
 
-#Create an input Asset
+# Create an input Asset
 print("Creating input asset " + in_asset_name)
-#From SDK
-#create_or_update(resource_group_name, account_name, asset_name, parameters, custom_headers=None, raw=False, **operation_config)
+# From SDK
+# create_or_update(resource_group_name, account_name, asset_name, parameters, custom_headers=None, raw=False, **operation_config)
 inputAsset = client.assets.create_or_update(resource_group_name, account_name, in_asset_name, input_asset)
 
-#An AMS asset is a container with a specfic id that has "asset-" prepended to the GUID.
-#So, you need to create the asset id to identify it as the container
-#where Storage is to upload the video (as a block blob)
+# An AMS asset is a container with a specfic id that has "asset-" prepended to the GUID.
+# So, you need to create the asset id to identify it as the container
+# where Storage is to upload the video (as a block blob)
 in_container = 'asset-' + inputAsset.asset_id
 
-#create an output Asset
+# create an output Asset
 print("Creating output asset " + out_asset_name)
-#From SDK
-#create_or_update(resource_group_name, account_name, asset_name, parameters, custom_headers=None, raw=False, **operation_config)
+# From SDK
+# create_or_update(resource_group_name, account_name, asset_name, parameters, custom_headers=None, raw=False, **operation_config)
 outputAsset = client.assets.create_or_update(resource_group_name, account_name, out_asset_name, output_asset)
 
 ### Use the Storage SDK to upload the video ###
 print("Uploading the file " + source_file)
-#From SDK
-#BlobServiceClient(account_url, credential=None, **kwargs)
+# From SDK
+# BlobServiceClient(account_url, credential=None, **kwargs)
 blob_service_client = BlobServiceClient(account_url=storage_blob_url, credential=storage_account_key)
-#From SDK
-#get_blob_client(container, blob, snapshot=None)
+# From SDK
+# get_blob_client(container, blob, snapshot=None)
 blob_client = blob_service_client.get_blob_client(in_container,source_file)
 # Upload the video to storage as a block blob
 with open(source_file, "rb") as data:
-  #From SDK
-  #upload_blob(data, blob_type=<BlobType.BlockBlob: 'BlockBlob'>, length=None, metadata=None, **kwargs)
+  # From SDK
+  # upload_blob(data, blob_type=<BlobType.BlockBlob: 'BlockBlob'>, length=None, metadata=None, **kwargs)
     blob_client.upload_blob(data, blob_type="BlockBlob")
 
 ### Create a Transform ###
 transform_name='MyTrans' + str(thisRandom)
-#From SDK
-#TransformOutput(*, preset, on_error=None, relative_priority=None, **kwargs) -> None
+# From SDK
+# TransformOutput(*, preset, on_error=None, relative_priority=None, **kwargs) -> None
 transform_output = TransformOutput(preset=BuiltInStandardEncoderPreset(preset_name="AdaptiveStreaming"))
 print("Creating transform " + transform_name)
-#From SDK
-#create_or_update(resource_group_name, account_name, transform_name, outputs, description=None, custom_headers=None, raw=False, **operation_config)
+# From SDK
+# Create_or_update(resource_group_name, account_name, transform_name, outputs, description=None, custom_headers=None, raw=False, **operation_config)
 transform = client.transforms.create_or_update(resource_group_name=resource_group_name,account_name=account_name,transform_name=transform_name,outputs=[transform_output])
 
 ### Create a Job ###
 job_name = 'MyJob'+ str(thisRandom)
 print("Creating job " + job_name)
 files = (source_file)
-#From SDK
-#JobInputAsset(*, asset_name: str, label: str = None, files=None, **kwargs) -> None
+# From SDK
+# JobInputAsset(*, asset_name: str, label: str = None, files=None, **kwargs) -> None
 input = JobInputAsset(asset_name=in_asset_name)
-#From SDK
-#JobOutputAsset(*, asset_name: str, **kwargs) -> None
+# From SDK
+# JobOutputAsset(*, asset_name: str, **kwargs) -> None
 outputs = JobOutputAsset(asset_name=out_asset_name)
-#From SDK
-#Job(*, input, outputs, description: str = None, priority=None, correlation_data=None, **kwargs) -> None
+# From SDK
+# Job(*, input, outputs, description: str = None, priority=None, correlation_data=None, **kwargs) -> None
 theJob = Job(input=input,outputs=[outputs])
-#From SDK
-#create(resource_group_name, account_name, transform_name, job_name, parameters, custom_headers=None, raw=False, **operation_config)
+# From SDK
+# Create(resource_group_name, account_name, transform_name, job_name, parameters, custom_headers=None, raw=False, **operation_config)
 job: Job = client.jobs.create(resource_group_name,account_name,transform_name,job_name,parameters=theJob)
 
 ### Check the progress of the job ### 
 # From SDK
 # get(resource_group_name, account_name, transform_name, job_name, custom_headers=None, raw=False, **operation_config)
 job_state = client.jobs.get(resource_group_name,account_name,transform_name,job_name)
-#First check
+# First check
 print("First job check")
 print(job_state.state)
 
-#Check the state of the job every 10 seconds. Adjust time_in_seconds = <how often you want to check for job state>
+# Check the state of the job every 10 seconds. Adjust time_in_seconds = <how often you want to check for job state>
 def countdown(t):
     while t: 
         mins, secs = divmod(t, 60) 
