3578d6d7f67a877236940a8bfc6ea48328a46208
IngridAtMicrosoft <inhenkel@microsoft.com>
2022-03-30 13:21:07 -0700

    assets script

--- all/assets.py
+++ all/assets.py
@@ -7,46 +7,54 @@ from azure.mgmt.media.models import (Asset)
 import os
 #</AssetImports>
 
-#<ClientEnvironmentVariables>
+#<EnvironmentVariables>
 #Get environment variables
 load_dotenv()
 
-# Get the default Azure credential from the environment variables AZURE_CLIENT_ID and AZURE_CLIENT_SECRET and AZURE_TENTANT_ID
-default_credential = DefaultAzureCredential()
+subscriptionId = os.getenv("SUBSCRIPTIONID")
+accountName=os.getenv("ACCOUNTNAME")
+resourceGroupName=os.getenv("RESOURCEGROUP")
+clientId = os.getenv("AZURE_CLIENT_ID")
+storageAccountName=os.getenv("STORAGEACCOUNTNAME")
+#</EnvironmentVariables>
 
-# The AMS Client
-client = AzureMediaServices(default_credential, os.getenv("SUBSCRIPTIONID"))
-#</ClientEnvironmentVariables>
+# Create the Media Services client and authenticate using the DefaultAzureCredential
+default_credential = DefaultAzureCredential()
+client = AzureMediaServices(default_credential, subscriptionId)
 
 #<CreateAsset>
-def createAsset(account_name, resource_group_name, asset_name, alternate_id=None, description=None, storage_account=None, container=None):
-  assetObj = Asset(alternate_id=alternate_id, description=description, storage_account_name=storage_account,container=container)
-  thisAsset = client.assets.create_or_update(os.getenv("RESOURCEGROUP"), os.getenv("ACCOUNTNAME"), asset_name, assetObj)
+#Create an Asset object
+#From SDK
+# Asset(*, alternate_id: Optional[str] = None, description: Optional[str] = None, container: Optional[str] = None,
+# storage_account_name: Optional[str] = None, **kwargs)
+assetName = "MyAsset"
+assetObj = Asset(alternate_id="myAlternateId",description="My description")
 
-createAsset(os.getenv("ACCOUNTNAME"),os.getenv("RESOURCEGROUP"),"myAsset","myAssetAlternateId","myAssetAltDesc")
+#From SDK
+#create_or_update(resource_group_name: str, account_name: str, asset_name: str, parameters: "_models.Asset", **kwargs: Any) -> _models.Asset
+
+def createAsset(account_name, resource_group_name, asset_name,asset):
+  thisAsset = client.assets.create_or_update(account_name, resource_group_name, asset_name,asset)
+
+#createAsset(resourceGroupName,accountName,assetName,assetObj)
 #</CreateAsset>
-
-#<DeleteAsset>
-def deleteAsset(resource_group,account_name,asset_name):
-  client.assets.delete(resource_group,account_name,asset_name)
-
-deleteAsset(os.getenv("RESOURCEGROUP"),os.getenv("ACCOUNTNAME"),"myAsset")
-#</DeleteAsset>
 
 #<GetAsset>
 def getAsset(resource_group_name,account_name,asset_name):
   results = client.assets.get(resource_group_name,account_name,asset_name)
-  print(results)
+  #You can get any of the properties of an asset. Here we are printing the asset name.
+  print(results.name)
 
-getAsset(os.getenv("RESOURCEGROUP"),os.getenv("ACCOUNTNAME"),"myAsset")
+#getAsset(resourceGroupName,accountName,assetName)
 #</GetAsset>
 
 #<GetAssetEncryptionKey>
 def getAssetEncKey(resource_group_name,account_name,asset_name):
+  #If an encryption key doesn't exist yet, the results will tell you.
   results = client.assets.get_encryption_key(resource_group_name,account_name,asset_name)
   print(results)
 
-getAssetEncKey(os.getenv("RESOURCEGROUP"),os.getenv("ACCOUNTNAME"),"myAsset")
+#getAssetEncKey(resourceGroupName,accountName,assetName)
 #</GetAssetEncryptionKey>
 
 #<ListAssets>
@@ -56,7 +64,7 @@ def listAssets(resource_group_name, account_name):
   for result in results:
     print(result.name)
 
-listAssets(os.getenv("RESOURCEGROUP"),os.getenv("ACCOUNTNAME"))
+#listAssets(resourceGroupName,accountName)
 ##</ListAssets>
 
 #<ListAssetsContainerSAS>
@@ -69,20 +77,26 @@ def listStreamingLocators(resource_group_name, account_name, asset_name):
   results=client.assets.list_streaming_locators(resource_group_name,account_name,asset_name)
   streamingLocators = results.streaming_locators
   # streaminglocators is a list so you can iterate over it to get an attribute of the streaming locator
+  # If no streaming locators have been created for the asset, then the results will return nothing.
   for locator in streamingLocators:
     print(locator.name)
 
-listStreamingLocators(os.getenv("RESOURCEGROUP"),os.getenv("ACCOUNTNAME"),"myAsset")
+listStreamingLocators(resourceGroupName,accountName,assetName)
 #</ListAssetStreamingLocators>
 
 #<UpdateAsset>
-params = {
-  "properties": {
-    "description": "I know you are but what am I?"
-  }
-}
-def updateAsset(resource_group_name, account_name, asset_name,parameters):
-  client.assets.update(resource_group_name,account_name,asset_name,parameters)
 
-updateAsset(os.getenv("RESOURCEGROUP"),os.getenv("ACCOUNTNAME"),"myAsset",params)
+assetObj1 = Asset(description="My new description.")
+
+def updateAsset(resource_group_name, account_name, asset_name,parameters):
+  client.assets.update(resource_group_name,account_name,asset_name,assetObj1)
+
+#updateAsset(resourceGroupName,accountName,assetName,assetObj1)
 #</UpdateAsset>
+
+#<DeleteAsset>
+def deleteAsset(resource_group,account_name,asset_name):
+  client.assets.delete(resource_group,account_name,asset_name)
+
+deleteAsset(resourceGroupName,accountName,assetName)
+#</DeleteAsset>
